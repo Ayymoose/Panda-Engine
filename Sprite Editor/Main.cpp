4a7092605f0e18049f0d0c40759f183dfffdf407
Moose <aymanm95@gmail.com>
2024-01-09 23:58:34 +0000

    add dark theme

--- Sprite Editor/Main.cpp
+++ Sprite Editor/Main.cpp
@@ -1,21 +1,41 @@
 #include "MainWindow.h"
-#include "NewLayer.h"
-#include "TransparencyColour.h"
 
 #include <QApplication>
+#include <QPalette>
+#include <QStyleFactory>
+
+void setDarkTheme()
+{
+    // Set Fusion style (modern style) for a consistent look on different platforms
+    qApp->setStyle(QStyleFactory::create("Fusion"));
+
+    // Fusion palette customization
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(45, 45, 45));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(25, 25, 25));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ToolTipBase, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    qApp->setPalette(darkPalette);
+
+    // Set the dark style for Fusion
+    qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    NewLayer nl(&w);
-    nl.show();
-
-    TransparencyColour tc(&w);
-    tc.show();
-
-
+    setDarkTheme();
+    MainWindow mw;
+    mw.show();
     return a.exec();
 }
