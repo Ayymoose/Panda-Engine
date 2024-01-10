619edb46645387dbdf31f3eeb273b00434cbd1fb
Moose <aymanm95@gmail.com>
2024-01-10 00:35:42 +0000

    add title for MainWindow

--- Sprite Editor/MainWindow.cpp
+++ Sprite Editor/MainWindow.cpp
@@ -1,11 +1,12 @@
 #include "MainWindow.h"
 #include "./ui_MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
+MainWindow::MainWindow(const QString& title, QWidget *parent)
     : QMainWindow(parent)
     , m_ui(std::make_unique<Ui::MainWindow>())
 {
     m_ui->setupUi(this);
+    setWindowTitle(title);
 }
 
 MainWindow::~MainWindow() = default;
